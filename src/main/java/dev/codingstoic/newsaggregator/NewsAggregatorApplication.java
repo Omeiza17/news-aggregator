64fb9a5757b077e1c1e2dd50470c812eda6ef4c1
Suleiman Michael <mosuleiman19@gmail.com>
2026-01-31 13:00:15 +0400

    feat: implement distributed caching with Redis and Cache-Aside pattern"

--- src/main/java/dev/codingstoic/newsaggregator/NewsAggregatorApplication.java
+++ src/main/java/dev/codingstoic/newsaggregator/NewsAggregatorApplication.java
@@ -2,11 +2,14 @@ package dev.codingstoic.newsaggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+
+@EnableCaching
 @SpringBootApplication
 public class NewsAggregatorApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(NewsAggregatorApplication.class, args);
 	}
 
